492d7340942eda361a45a51fb337eef74594c06a
lordeber15 <eberpalomino@hotmail.com>
2025-01-11 19:02:42 -0500

    cuatro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+
 function App() {
   
   return (
     <div className='w-screen rounded-sm'>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <Viewer fileUrl="../src/document/carta.pdf" />;
+      <Viewer fileUrl="../public/carta.pdf" />;
       </Worker>
     </div>
   )
